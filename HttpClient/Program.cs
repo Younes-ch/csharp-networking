d62dc2c912c33a2a23d597f8dcb0d7cf3ff9d416
Younes-ch <younes.jmell10@gmail.com>
2024-12-26 16:55:20 +0100

    Add HttpClient project.

---
+++ HttpClient/Program.cs
@@ -0,0 +1,34 @@
+﻿const string urlToDownload = "https://example.com";
+const string fileName = "index.html";
+
+var webPageDownloaderService = new WebPageDownloaderService(urlToDownload, fileName);
+var downloadTask = webPageDownloaderService.DownloadWebPage();
+Console.WriteLine("Holding for at least 5 seconds...");
+Thread.Sleep(TimeSpan.FromSeconds(5));
+
+downloadTask.GetAwaiter().GetResult();
+
+internal class WebPageDownloaderService(string url, string fileName)
+{
+    public async Task DownloadWebPage()
+    {
+        Console.WriteLine("Starting download...");
+
+        using var httpClient = new HttpClient();
+
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Fetched the webpage...");
+
+            var data = await response.Content.ReadAsByteArrayAsync();
+
+            var fileStream = File.Create(fileName);
+            await fileStream.WriteAsync(data);
+            fileStream.Close();
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
---
+++ HttpClient/README.md
@@ -0,0 +1,4 @@
+﻿# [HttpClient](https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=net-9.0)
+
+HttpClient works by establishing a connection over TCP to get a response from any server that serves web pages (or responds to HTTP requests).
+It's pretty simple to use, but you need to use asynchronous code.
