ea69487b364a2e8254c99edde08fec1d29d72628
Younes-ch <younes.jmell10@gmail.com>
2024-12-27 18:34:48 +0100

    Refactor: renamed private methods.

--- TcpChat/TcpChatServer/Program.cs
+++ TcpChat/TcpChatServer/Program.cs
@@ -86,91 +86,8 @@ internal class TcpChatServer
 
         Console.WriteLine("Server is shut down.");
     }
-            var msg = Encoding.UTF8.GetString(msgBuffer, 0, bytesRead);
-
-    // Clears out the message queue and sends it to all viewers
-    private void SendMessages()
-    {
-        foreach (var msg in _messageQueue)
-        {
-            // Encode the message
-            var msgBuffer = Encoding.UTF8.GetBytes(msg);
-
-            // Send the message to each viewer
-            foreach (var viewer in _viewers) viewer.GetStream().Write(msgBuffer);
-        }
-
-        _messageQueue.Clear();
-    }
-
-    // See if any of our messengers have sent us a new message, put it in the queue
-    private void CheckForNewMessages()
-    {
-        foreach (var messenger in _messengers)
-        {
-            var messageLength = messenger.Available;
-            if (messageLength > 0)
-            {
-                var msgBuffer = new byte[messageLength];
-                var _ = messenger.GetStream().Read(msgBuffer, 0, messageLength);
-
-                var msg = $"{_names[messenger]}: {Encoding.UTF8.GetString(msgBuffer)}";
-                _messageQueue.Enqueue(msg);
-            }
-        }
-    }
-
-    private void CheckForDisconnects()
-    {
-        // Check the viewers first
-        foreach (var viewer in _viewers.ToList())
-            if (IsDisconnected(viewer))
-            {
-                Console.WriteLine($"Viewer {viewer.Client.RemoteEndPoint} has left.");
-
-                _viewers.Remove(viewer);
-                CleanupClient(viewer);
-            }
-
-        // Check the messengers second
-        foreach (var messenger in _messengers.ToList())
-            if (IsDisconnected(messenger))
-            {
-                // Get info about the messenger
-                var name = _names[messenger];
-
-                // Tell the viewers someone has left
-                Console.WriteLine($"Messenger {messenger.Client.RemoteEndPoint} has left.");
-                _messageQueue.Enqueue($"{name} has left the chat.");
-
-                // Clean up on our end
-                _messengers.Remove(messenger);
-                _names.Remove(messenger);
-                CleanupClient(messenger);
-            }
-    }
-
-    // Checks if a socket has disconnected
-    private static bool IsDisconnected(TcpClient client)
-    {
-        try
-        {
-            var socket = client.Client;
-            return socket.Poll(10 * 1000, SelectMode.SelectRead) && socket.Available == 0;
-        }
-        catch
-        {
-            return true;
-        }
-    }
-
-    private static void CleanupClient(TcpClient client)
-    {
-        client.GetStream().Close();
-        client.Close();
-    }
-
-    private void HandleNewConnection()
+    
+     private void _handleNewConnection()
     {
         var good = false;
         var newClient = _listener.AcceptTcpClient(); // Blocks Thread
@@ -191,7 +108,7 @@ internal class TcpChatServer
 
         if (bytesRead > 0)
         {
-            var msg = Encoding.UTF8.GetString(msgBuffer);
+            var msg = Encoding.UTF8.GetString(msgBuffer, 0, bytesRead);
 
             if (msg == "viewer")
             {
@@ -230,12 +147,94 @@ internal class TcpChatServer
             {
                 // Wasn't either a viewer or messenger, clean up anyway.
                 Console.WriteLine($"Wasn't able to identify {endPoint} as a Viewer or Messenger.");
-                CleanupClient(newClient);
+                _cleanupClient(newClient);
             }
         }
 
         // Do we really want them?
         if (!good)
             newClient.Close();
+    }
+
+    // Clears out the message queue and sends it to all viewers
+    private void _sendMessages()
+    {
+        foreach (var msg in _messageQueue)
+        {
+            // Encode the message
+            var msgBuffer = Encoding.UTF8.GetBytes(msg);
+
+            // Send the message to each viewer
+            foreach (var viewer in _viewers) viewer.GetStream().Write(msgBuffer);
+        }
+
+        _messageQueue.Clear();
+    }
+
+    // See if any of our messengers have sent us a new message, put it in the queue
+    private void _checkForNewMessages()
+    {
+        foreach (var messenger in _messengers)
+        {
+            var messageLength = messenger.Available;
+            if (messageLength > 0)
+            {
+                var msgBuffer = new byte[messageLength];
+                var _ = messenger.GetStream().Read(msgBuffer, 0, messageLength);
+
+                var msg = $"{_names[messenger]}: {Encoding.UTF8.GetString(msgBuffer)}";
+                _messageQueue.Enqueue(msg);
+            }
+        }
+    }
+
+    private void _checkForDisconnects()
+    {
+        // Check the viewers first
+        foreach (var viewer in _viewers.ToList())
+            if (_isDisconnected(viewer))
+            {
+                Console.WriteLine($"Viewer {viewer.Client.RemoteEndPoint} has left.");
+
+                _viewers.Remove(viewer);
+                _cleanupClient(viewer);
+            }
+
+        // Check the messengers second
+        foreach (var messenger in _messengers.ToList())
+            if (_isDisconnected(messenger))
+            {
+                // Get info about the messenger
+                var name = _names[messenger];
+
+                // Tell the viewers someone has left
+                Console.WriteLine($"Messenger {messenger.Client.RemoteEndPoint} has left.");
+                _messageQueue.Enqueue($"{name} has left the chat.");
+
+                // Clean up on our end
+                _messengers.Remove(messenger);
+                _names.Remove(messenger);
+                _cleanupClient(messenger);
+            }
+    }
+
+    // Checks if a socket has disconnected
+    private static bool _isDisconnected(TcpClient client)
+    {
+        try
+        {
+            var socket = client.Client;
+            return socket.Poll(10 * 1000, SelectMode.SelectRead) && socket.Available == 0;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+
+    private static void _cleanupClient(TcpClient client)
+    {
+        client.GetStream().Close();
+        client.Close();
     }
 }
